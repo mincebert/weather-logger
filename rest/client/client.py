42a61402ca2ad0b0aa98a592b8bf002f097bd910
Robert Franklin <rcf@mince.net>
2021-04-18 22:35:13 +0100

    got client working

--- rest/client/client.py
+++ rest/client/client.py
@@ -2,11 +2,25 @@
 
 import requests
 import json
+import time
 
-response = requests.get('http://shizuka.local:8123/weather/latest')
-if response:
-    d = response.json()
-    for s in d:
-        print(s, "=>", d[s])
-else:
-    print('Problem GETting!')
+import scrollphathd as sphd
+import thermofont
+
+sphd.set_font(thermofont)
+
+while True:
+    response = requests.get("http://shizuka.local:8123/weather/latest")
+    if response:
+        d = response.json()
+        for s in d:
+            print(s, "=>", d[s])
+    else:
+        print('Problem GETting!')
+
+    sphd.clear()
+    # need to handle -ve
+    sphd.write_string("%4.1f'" % d["1"]["temp"], brightness=0.3)
+    sphd.show()
+
+    time.sleep(60)

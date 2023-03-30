14fc72c36d389115ce86976afdcaf1aeff7743ec
Robert Franklin <rcf@mince.net>
2023-03-31 00:08:00 +0100

    reorganised data around locations not sensors
    this abstracts the sensor numbers from the locations and allows them to
    be moved around, swapping them or moving them to new locations and not
    having the data muddled with previous locations

--- sql/weather.sql
+++ sql/weather.sql
@@ -1,10 +1,15 @@
+-- ---------------------------------------------------------------------------
+-- SENSORS AND LOCATIONS
+-- ---------------------------------------------------------------------------
+
+
 -- create the sensor locations table
 --
--- This describes the locations (current or historical) of the sensors.
+-- This describes the locations (current or historical) of data.
 
-CREATE TABLE locations (
-  location varchar PRIMARY KEY,
-  description varchar NOT NULL
+CREATE TABLE location (
+  id integer PRIMARY KEY CHECK (id >= 0),
+  description varchar UNIQUE NOT NULL
 );
 
 
@@ -12,16 +17,21 @@ CREATE TABLE locations (
 --
 -- This maps current sensor numbers to locations.
 
-CREATE TABLE sensors (
+CREATE TABLE sensor_location (
   sensor integer PRIMARY KEY CHECK (sensor >= 0),
-  location varchar UNIQUE REFERENCES locations (location) NOT NULL
+  location integer UNIQUE NOT NULL REFERENCES location (id)
 );
+
+
+-- ---------------------------------------------------------------------------
+-- WEATHER LATEST
+-- ---------------------------------------------------------------------------
 
 
 -- create the 'latest' (current) weather table
 
 CREATE TABLE weather_latest (
-  location varchar PRIMARY KEY REFERENCES sensors (location),
+  location integer REFERENCES location (id) PRIMARY KEY,
 
   datetime timestamp with time zone,
   temp numeric(4, 1),
@@ -42,36 +52,69 @@ CREATE FUNCTION weather_latest_update(
   new_temp numeric(4, 1),
   new_humidity integer
 ) RETURNS void AS $$
+DECLARE
+  new_location integer;
 BEGIN
-  -- find out of there's a row already for this sensor
-  IF (SELECT COUNT(*) FROM weather_latest WHERE sensor = new_sensor) = 0 THEN
+  new_location = (SELECT location FROM sensor_location
+                    WHERE sensor = new_sensor);
+
+  -- find out of there's a row already for this location
+  IF (SELECT COUNT(*) FROM weather_latest
+	WHERE location = new_location) = 0 THEN
     -- no row already - just store this one as the first
 
-    INSERT INTO weather_latest (datetime, sensor, temp, humidity)
-      SELECT new_datetime, new_sensor, new_temp, new_humidity;
+    INSERT INTO weather_latest (datetime, location, temp, humidity)
+      SELECT new_datetime, new_location, new_temp, new_humidity;
 
   -- see if this row is newer than the one already stored
   ELSIF new_datetime > (SELECT datetime FROM weather_latest
-                          WHERE sensor = new_sensor) THEN
+                          WHERE location = new_location) THEN
 
     -- it's newer - replace the existing row
     UPDATE weather_latest
       SET datetime = new_datetime, temp = new_temp, humidity = new_humidity
-      WHERE sensor = new_sensor;
+      WHERE location = new_location;
 
   END IF;
 END
 $$ LANGUAGE plpgsql;
 
 
+-- ---------------------------------------------------------------------------
+-- WEATHER HISTORY
+-- ---------------------------------------------------------------------------
+
+
 -- create the historical data table
 
 CREATE TABLE weather_history (
-  sensor integer CHECK (sensor >= 0),
   datetime timestamp with time zone,
+  location integer REFERENCES location (id),
 
   temp numeric(4, 1),
   humidity integer,
 
-  PRIMARY KEY (datetime, sensor)
+  PRIMARY KEY (datetime, location)
 );
+
+
+-- weather_history_insert()
+--
+-- This function adds data to the weather_history table with new information.
+
+CREATE FUNCTION weather_history_insert(
+  new_sensor integer,
+  new_datetime timestamp,
+  new_temp numeric(4, 1),
+  new_humidity integer
+) RETURNS void AS $$
+DECLARE
+  new_location integer;
+BEGIN
+  new_location = (SELECT location FROM sensor_location
+                    WHERE sensor = new_sensor);
+
+  INSERT INTO weather_history (location, datetime, temp, humidity)
+    VALUES (new_location, new_datetime, new_temp, new_humidity);
+END
+$$ LANGUAGE plpgsql;
